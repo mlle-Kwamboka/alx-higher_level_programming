ffb3f417f5b85dcefb21973a86eed05e78cd3b7e
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-03 01:06:07 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,9 @@
 
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    all_stuff = dir()
+    import hidden_4
+    
+    all_stuff = dir(hidden_4)
     for i in range(1, len(all_stuff)):
         if all_stuff[i][0:2] != "__":
             print("{}".format(all_stuff[i]))
