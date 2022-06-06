c9d7a73090e78835d08687aa2bba70931f862565
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-06 23:37:26 +0300

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if type(my_list[i]) = int:
-            print("{}".format(my_list[]))
+        print("{}".format(my_list[]))
