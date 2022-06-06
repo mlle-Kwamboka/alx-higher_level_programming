f3bee897bd7e96da75cab5962eb05e4df2c0d71f
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-06 23:45:02 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        idx = my_list.index(i)
-        if idx < 0:
+    if idx < 0:
+        return 'None'
+    elif idx > len(my_list[i] - 1):
             return 'None'
-        elif idx > len(my_list[i] - 1):
-            return 'None'
-        else:
+    else:
             return my_list[idx]
