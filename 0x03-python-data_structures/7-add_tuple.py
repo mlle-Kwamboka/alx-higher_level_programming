2514e9a8b358c303f1cdc435ce43d737dffd81c3
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-07 00:18:45 +0300

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i in tuple_a:
-        for j in tuple_b:
-            tuple_c = (tuple_a[i] + tuple_b[j])
-            if len(tuple_a[i]), len(tuple_b[j]) < 2:
-                tuple_a[i + 1], tuple_b[j + 1] = 0
-            elif len(tuple_a[i]), len(tuple_b[j]) > 2:
-                i, j  = 1, 1
-            else:
-                print("{}".format(tuple_c))
+    tuple_3 = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    tuple_3 = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return tuple_3
