e2c8d71fd315aad5baf7b5187add0cc7b0077611
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-06-23 01:54:32 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """initializing instance size"""
 
-        size.__self = size
+        self.__Size = size
 
     @property
     def size(self):
