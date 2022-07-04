61d6f25e0b01fd116ef0d42534cd206438181688
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-07-05 00:01:20 +0300

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module 1-my_list
+A class that inherits from list
+"""
+
+class MyList(list):
+    """Class MyList inherits from list"""
+    
+    def print_sorted(self):
+        """prints list sorted in ascending order"""
+        new_list = [:]
+        new_list.sort()
+        print("{}".format(new_list))
