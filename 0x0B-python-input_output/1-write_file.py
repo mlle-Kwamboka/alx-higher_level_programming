3615b49761933a01936f4e0eb2c553a8d18a9e78
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-07-06 00:51:50 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,12 +3,15 @@
 returns the number of characters written
 """
 
+
 def write_file(filename="", text=""):
     """Writes a string to a text file
+    
     Args:
         filename: The file
         text: The string
     Return: Number of characters
     """
+    
     with open(filename, 'w+') as f:
         return f.write(text)
