75d75b8918e2846ab6f6cb7f394c5ea61c04c47f
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-07-15 17:23:25 +0300

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,1 +1,44 @@
+#!/usr/bin/python3
+"""unittest base.
+test cases for base class
+Each test has a number corresponding to task: eg test_1_0 corresponds to task
+number 1
+"""
 
+import unittest
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class TestBase(unittest.TestCase):
+    """test class for Base class"""
+
+    def setUp(self):
+         Base.Base.__nb_objects = 0
+
+    def test_1_0(self):
+        """Creates new Instances: check for id"""
+        b0 = Base()
+        self.assertEqual(b0.id, 1)
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
+        b2 = Base()
+        self.assertEqual(b2.id, 3)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base(4)
+        self.assertEqual(b4.id, 4)
+        b5 = Base(-5)
+        self.assertEqual(b5.id, -5)
+        b6 = Base(95)
+        self.assertEqual(b6.id, 96)
+
+    def test_1_1(self):
+        """tests for type and Instance"""
+        b6 = Base()
+        self.assertEqual(type(b6), Base)
+        self.assertTrue(isinstance(b6, Base))
+        
+if __name__ == '__main__':
+    unittest.main()
