9f6381c8d7d8c5ef085913d2089de8d91dab5ae3
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-07-15 17:47:04 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""unittest rectangle
-test cases for the rectangle class numbered accoding to task number
-eg test_2_2: test 2 of task 2
+"""Unittest rectangle
+Test cases for the Rectangle class.
+Each test has the number of the task,
+and the number of the test for that task
+(i.e 'test_17_0' for the first test of task 17)
 """
 
 import unittest
@@ -11,14 +13,15 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
+
 class TestRectangle(unittest.TestCase):
-    """Test case for the rectangle class"""
+    """Test cases for the Rectangle class."""
 
     def setUp(self):
         Base._Base__nb_objects = 0
 
     def test_2_0(self):
-        """Test for rectangle: checking id"""
+        """Test Rectangle class: check for id."""
 
         r0 = Rectangle(1, 2)
         self.assertEqual(r0.id, 1)
@@ -26,29 +29,29 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.id, 2)
         r2 = Rectangle(3, 4)
         self.assertEqual(r2.id, 3)
-        r3 = Rectangle(10, 1, 0, 0, 12)
+        r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
-        r4 = Rectangle(5, 2, 0, 1, 9)
-        self.assertEqual(r4.id, 9)
-        r5 = Rectangle(6, 2, 1, 1, -3)
+        r4 = Rectangle(10, 2, 4, 5, 34)
+        self.assertEqual(r4.id, 34)
+        r5 = Rectangle(10, 2, 4, 5, -5)
         self.assertEqual(r5.id, -5)
-        r6 = Rectangle(10, 2, 0, 3, 25)
-        self.assertEqual(r6.id, 25)
-
+        r6 = Rectangle(10, 2, 4, 5, 9)
+        self.assertEqual(r6.id, 9)
 
     def test_2_1(self):
-        """Test for attribute values"""
-        r0 = Rectangle(5, 2)
-        self.assertEqual(r0.width, 5)
-        self.assertEqual(r0.height, 2)
-        self.assertEqual(r0.x, 0)
-        self.assertEqual(r0.y, 0)
-        r1 = Rectangle(5, 2, 3, 4, 25)
-        self.assertEqual(r1.width, 5)
+        """Test Rectangle class: check for attributes values."""
+
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 3)
-        self.assertEqual(r1.y, 4)
-        
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        r2 = Rectangle(10, 2, 4, 5, 24)
+        self.assertEqual(r2.width, 10)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 4)
+        self.assertEqual(r2.y, 5)
+
     def test_2_2(self):
         """Test class Rectangle: check for missing arguments."""
 
@@ -75,10 +78,10 @@ class TestRectangle(unittest.TestCase):
         """Test Rectangle class: check for wrong attributes."""
 
         with self.assertRaises(TypeError) as x:
-            r = Rectangle("School", 2)
+            r = Rectangle("Hello", 2)
         self.assertEqual("width must be an integer", str(x.exception))
         with self.assertRaises(TypeError) as x:
-            r = Rectangle(2, "Fun")
+            r = Rectangle(2, "World")
         self.assertEqual("height must be an integer", str(x.exception))
         with self.assertRaises(TypeError) as x:
             r = Rectangle(1, 2, "Foo", 3)
@@ -101,16 +104,16 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError) as x:
             r = Rectangle(2, 8, 9, -65)
         self.assertEqual("y must be >= 0", str(x.exception))
-        
+
     def test_4_0(self):
         """Test for public method area with normal types."""
 
-        r1 = Rectangle(3, 4)
-        self.assertEqual(r1.area(), 12)
-        r2 = Rectangle(25, 2)
-        self.assertEqual(r2.area(), 50)
-        r3 = Rectangle(5, 7, 0, 0, 12)
-        self.assertEqual(r3.area(), 35)
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+        r2 = Rectangle(75, 2)
+        self.assertEqual(r2.area(), 150)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
 
     def test_4_1(self):
         """Test for public method area with wrong args."""
@@ -121,7 +124,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(
             "area() takes 1 positional argument but 2 were given", str(
                 x.exception))
-        
+
     def test_5_0(self):
         """Test for public method display."""
 
@@ -243,4 +246,3 @@ class TestRectangle(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
