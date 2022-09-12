9468d165aebe31a2961a16c486a1107d2dec1fe5
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-09-12 20:10:39 +0400

    Update 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const square = require('./5-square.js')
+
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
