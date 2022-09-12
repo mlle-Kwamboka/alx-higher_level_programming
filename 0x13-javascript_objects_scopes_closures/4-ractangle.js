10ec00965ea01ba108ca1bb747c87f1c601752eb
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-09-12 18:47:57 +0400

    Update 4-ractangle.js

--- 0x13-javascript_objects_scopes_closures/4-ractangle.js
+++ 0x13-javascript_objects_scopes_closures/4-ractangle.js
@@ -7,19 +7,22 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
+
   double () {
     const doubleHeight = this.height * 2;
     const doubleWidth = this.width * 2;
   }
 }
-const rec1 = new Rectangle(4, 3);
+module.exports = Rectangle;
