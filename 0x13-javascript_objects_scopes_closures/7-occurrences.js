2c867c819165fa0c917c2535074218232b5ca587
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-09-12 20:30:37 +0400

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,4 @@
-exports.nbOccurences = function (list, searchElement)
-  return (list.filter(elem => elem === searchElement).length);
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  return (list.filter(elem => elem === searchElement).length)
+};
