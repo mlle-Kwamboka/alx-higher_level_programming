2eed2dd7997ba735055db92eb04b792b4eb0e651
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-09-28 15:36:26 +0400

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+#Script that takes in a URL, request to that URL, and displays body size
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
